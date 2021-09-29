ba5359fd26f7d2260b6af237d42678d9a11456df
Axel Anaya <axel.ana25@gmail.com>
2021-09-29 17:51:31 -0500

    comentario removido

--- app/__init__.py
+++ app/__init__.py
@@ -29,11 +29,6 @@ def create_app(test_config=None):
     from app import alumnos
 
     app.register_blueprint(alumnos.bp)
-
-    # make url_for('index') == url_for('blog.index')
-    # in another app, you might define a separate main index here with
-    # app.route, while giving the blog blueprint a url_prefix, but for
-    # the tutorial the blog will be the main index
     app.add_url_rule("/", endpoint="index")
 
     return app
